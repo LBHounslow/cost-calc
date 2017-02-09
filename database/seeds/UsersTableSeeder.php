c3f4334e80e9f973e537f82c4fdebe46f7e8bb70
Jack Segal <jackbsegal@gmail.com>
2017-02-09 16:41:28 +0000

    Removing MADM Employees from database seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,10 +13,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-                'name' => 'Jack Segal',
-                'email' => 'jackbsegal@gmail.com',
+                'name' => 'Jane Doe',
+                'email' => 'jane.done@test.gov.uk',
                 'remember_token' => str_random(10),
-                'password' => bcrypt('HelloWorld'),
+                'password' => bcrypt('12345678'),
                 'permissions' => '["uploadFile","reports","clientLookup","settings"]',
                 'active' => '1',
                 'provider_code' => 'hounslow-madm',
@@ -25,56 +25,5 @@ class UsersTableSeeder extends Seeder
             ]
         );
 
-        DB::table('users')->insert([
-                'name' => 'Owen Kennedy',
-                'email' => 'owen.kennedy@hounslow.gov.uk',
-                'remember_token' => str_random(10),
-                'password' => bcrypt('HelloWorld'),
-                'permissions' => '["uploadFile","reports","clientLookup","settings"]',
-                'active' => '1',
-                'provider_code' => 'hounslow-madm',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]
-        );
-
-        DB::table('users')->insert([
-                'name' => 'Andy Strange',
-                'email' => 'andy.strange@hounslow.gov.uk',
-                'remember_token' => str_random(10),
-                'password' => bcrypt('HelloWorld'),
-                'permissions' => '["uploadFile","reports","clientLookup","settings"]',
-                'active' => '1',
-                'provider_code' => 'hounslow-madm',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]
-        );
-
-        DB::table('users')->insert([
-                'name' => 'Ingrid Karikari',
-                'email' => 'ingridkarikari@gmail.com',
-                'remember_token' => str_random(10),
-                'password' => bcrypt('HelloWorld'),
-                'permissions' => '["uploadFile","reports","clientLookup","settings"]',
-                'active' => '1',
-                'provider_code' => 'hounslow-madm',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]
-        );
-
-        DB::table('users')->insert([
-                'name' => 'Lucy Watt',
-                'email' => 'lucy.e.watt@gmail.com',
-                'remember_token' => str_random(10),
-                'password' => bcrypt('HelloWorld'),
-                'permissions' => '["uploadFile","reports","clientLookup","settings"]',
-                'active' => '1',
-                'provider_code' => 'hounslow-madm',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]
-        );
     }
 }
