33fa8bc60c7598d0aeecd019cdfcbbba29f368b1
Jack Segal <jackbsegal@gmail.com>
2017-02-24 13:39:53 +0000

    CheckUserStatus runs after successful login so updating success manually

--- app/Http/Middleware/CheckUserStatus.php
+++ app/Http/Middleware/CheckUserStatus.php
@@ -1,22 +1,32 @@
 <?php
 
 namespace App\Http\Middleware;
+
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use App\User_login_log;
+
 class CheckUserStatus
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $response = $next($request);
         //If the status is not active redirect to login
-        if(Auth::check() && Auth::user()->active != 1){
+        if (Auth::check() && Auth::user()->active != 1) {
+
+            User_login_log::where('user_id', Auth::user()->id)
+                ->latest()
+                ->first()
+                ->update(['success' => 0]);
+
+
             Auth::logout();
             return redirect('/login')->withErrors(['inactive_user' => 'Your account has been suspended']);
         }
