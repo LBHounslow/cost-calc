56c0aca1489d530c0d3586a5a5cd6b59c244c88b
Andy Strange <code@grit-oyster.co.uk>
2017-05-01 18:16:58 +0100

    De-Hounslowified the home screen text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts/app')
 
 @section('title')
-    Welcome to Hounslow Council’s Cost Calculator
+    Welcome to the Cost Calculator
 @endsection
 
 @section('content')
@@ -9,19 +9,12 @@
     <hr>
 
     <h3>About the Cost Calculator</h3>
-    <p>
-        This tool contains data from a variety of sources. It allows you to easily see and analyse what the council is
-        spending on individuals, groups and services. For further information please contact:
-    </p>
-    <br>
-    <p>
-        Vinesh Govind (<a
-                href="mailto:Vinesh.Govind@hounslow.gov.uk?subject=Cost Calculator info request">
-            Vinesh.Govind@hounslow.gov.uk</a>)
-    </p>
-    <p>
-        Fatima Ajia (<a
-                href="mailto:Fatima.Ajia@hounslow.gov.uk?subject=Cost Calculator info request">
-            Fatima.Ajia@hounslow.gov.uk</a>)
-    </p>
+    <p>The Cost Calculator is a web based intelligence tool designed to support local authorities in analysing the use of the services they provide. It uses the cost of service provision as the primary measure of service use.</p>
+    <p>The tool is capable of:</p>
+    <ul>
+      <li>processing data sets that contain financial information about the users of services;</li>
+      <li>matching individuals across the different data sets to produce a "client index", which is used to calculate all costs associated with those clients;</li>
+      <li>producing three types of reports about service use, which can be filtered in different ways to aid analysis.</li>
+    </ul>
+    <p>The objective of the tool is to facilitate those working in local authorities and other public services to be able to analyse financial information and to gain an understanding of the pattern of service use of an individual, or group of individuals, beyond one particular service.</p>
 @endsection
