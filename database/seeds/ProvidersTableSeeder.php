b46fda40b15042281597565e527f07cd808ecb22
Andy Strange <code@grit-oyster.co.uk>
2017-05-12 13:01:24 +0100

    Generic seeding of providers

--- database/seeds/ProvidersTableSeeder.php
+++ database/seeds/ProvidersTableSeeder.php
@@ -13,32 +13,8 @@ class ProvidersTableSeeder extends Seeder
     public function run()
     {
         DB::table('providers')->insert([
-                'display_name' => 'Hounslow MADM',
-                'allowed_file_types' => '["1","2", "3", "4", "5"]',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]
-        );
-
-        DB::table('providers')->insert([
-                'display_name' => 'Hounslow Housing',
-                'allowed_file_types' => '["1", "3", "4"]',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]
-        );
-
-        DB::table('providers')->insert([
-                'display_name' => 'Hounslow Adult Social Care',
-                'allowed_file_types' => '["2"]',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            ]
-        );
-
-        DB::table('providers')->insert([
-                'display_name' => 'West London Mental Health',
-                'allowed_file_types' => '[]',
+                'display_name' => 'Cost Calculator Admin',
+                'allowed_file_types' => '["1", "2", "3", "4", "5"]',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ]
