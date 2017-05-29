29a47b3b4d7bddb43b0f02a35498b54fab5f7b54
Jack Segal <jackbsegal@gmail.com>
2017-05-29 15:15:37 +0100

    Sprint 2 - Showing Personal Details on Client Lookup

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Clients;
 use View;
 use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
 
 class ClientController extends Controller
 {
@@ -49,8 +50,9 @@ class ClientController extends Controller
     public function show($id)
     {
         $client = Clients::find($id);
+        $details = $this->getClientDetails($id);
 
-        return View::make('clients/index', ['client' => $client]);
+        return View::make('clients/index', ['client' => $client, 'details' => $details]);
     }
 
     /**
@@ -85,5 +87,76 @@ class ClientController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function getClientDetails($id)
+    {
+        $query = "
+                                SELECT DISTINCT
+                                                client_id as 'client_id',
+                                                [first_name] as 'first_name', null as 'ni', [nhs_no] as 'nhs_no',
+                                                CONCAT([address_1],', ',[address_2],', ',[address_3],', ',[town],', ',[county],', ',[postcode]) as 'full_address'
+                                FROM
+                                                [import_adult_social_care_services]
+                                WHERE
+                                                client_id = $id
+
+                                UNION
+
+                                SELECT DISTINCT
+                                                client_id as 'client_id',
+                                                [first_name] as 'first_name', [ni] as 'ni', null as 'nhs_no',
+                                                CONCAT([address_1],', ',[address_2],', ',[address_3],', ',[address_4],', ',[postcode]) as 'full_address'
+                                FROM
+                                                [import_housing_temp_accom]
+                                WHERE
+                                                client_id = $id
+
+                                UNION
+
+                                SELECT DISTINCT
+                                                client_id as 'client_id',
+                                                [first_name] as 'first_name', [ni] as 'ni', [nhs_no] as 'nhs_no',
+                                                CONCAT([address_1],', ',[address_2],', ',[address_3],', ',[address_4],', ',[postcode]) as 'full_address'
+                                FROM
+                                                [import_general_services]
+                                WHERE
+                                                client_id = $id
+
+                                UNION
+
+                                SELECT DISTINCT
+                                                client_id as 'client_id',
+                                                [first_name] as 'first_name', [ni] as 'ni', null as 'nhs_no',
+                                                CONCAT([address_1],', ',[address_2],', ',[address_3],', ',[address_4],', ',[postcode]) as 'full_address'
+                                FROM
+                                                [import_housing_benefit_entitle]
+                                WHERE
+                                                client_id = $id
+
+                                UNION
+
+                                SELECT DISTINCT
+                                                client_id as 'client_id',
+                                                [first_name] as 'first_name', [ni] as 'ni', null as 'nhs_no',
+                                                [address] as 'full_address'
+                                FROM
+                                                [import_housing_benefit_switch]
+                                WHERE
+                                                client_id = $id";
+
+        return $this->executeQuery($query);
+    }
+
+    private function executeQuery($query)
+    {
+        if (env('DB_CONNECTION', false) == 'mysql') {
+            //$result = DB::select(DB::raw($query));
+            return [];
+        } else {
+            DB::connection()->setFetchMode(\PDO::FETCH_ASSOC);
+            $result = DB::select(DB::raw($query));
+            return $result;
+        }
     }
 }
--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -23,6 +23,110 @@
             </li>
             <li class="list-group-item"><strong>Postcode:</strong> {{ $client->postcode }}</li>
         </ul>
+
+
+        <ul class="list-group">
+            <li class="list-group-item"><strong>First Name:</strong>
+                @php
+                foreach($details as $detail) {
+                if(isset($unique)) {
+                if($unique == strtoupper($detail['first_name'])) {
+
+                } else {
+                echo " / ".$detail['first_name'];
+                }
+                } else {
+                $unique = strtoupper($detail['first_name']);
+                echo " ".$detail['first_name'];
+                }
+                }
+                @endphp
+            </li>
+        </ul>
+
+        <ul class="list-group">
+            <li class="list-group-item"><strong>NI:</strong>
+                @php
+                unset($unique);
+                foreach($details as $detail) {
+                if($detail['ni'] == '' || $detail['ni'] == 'N/A') {
+
+                } else {
+                if(isset($unique)) {
+                if($unique == strtoupper($detail['ni'])) {
+
+                } else {
+                echo " / ".$detail['ni'];
+                }
+                } else {
+                $unique = strtoupper($detail['ni']);
+                echo " ".$detail['ni'];
+                }
+                }
+                }
+                @endphp
+            </li>
+        </ul>
+
+        <ul class="list-group">
+            <li class="list-group-item"><strong>NHS No:</strong>
+                @php
+                unset($unique);
+                foreach($details as $detail) {
+                if($detail['nhs_no'] == '' || $detail['nhs_no'] == 'N/A') {
+
+                } else {
+                if(isset($unique)) {
+                if($unique == strtoupper($detail['nhs_no'])) {
+
+                } else {
+                echo " / ".$detail['nhs_no'];
+                }
+                } else {
+                $unique = strtoupper($detail['nhs_no']);
+                echo " ".$detail['nhs_no'];
+                }
+                }
+                }
+                @endphp
+            </li>
+        </ul>
+
+        <ul class="list-group">
+            <li class="list-group-item"><strong>Full Address:</strong>
+                <div class="row">
+                    @php
+                    unset($unique);
+                    foreach($details as $detail) {
+                    if($detail['full_address'] == '' || $detail['full_address'] == 'N/A') {
+
+                    } else {
+                    if(isset($unique)) {
+                    $compare = strtolower($detail['full_address']);
+                    $compare = str_replace(' ', '', $compare);
+                    $compare = str_replace(',', '', $compare);
+                    if($unique == $compare) {
+
+                    } else {
+                    echo '
+                    <div class="col-sm-3">'.$detail['full_address'].'</div>
+                    ';
+                    }
+                    } else {
+                    $unique = strtolower($detail['full_address']);
+                    $unique = str_replace(' ', '', $unique);
+                    $unique = str_replace(',', '', $unique);
+                    echo '
+                    <div class="col-sm-3">'.$detail['full_address'].'</div>
+                    ';
+                    }
+                    }
+                    }
+                    @endphp
+                </div>
+            </li>
+        </ul>
+
     @endif
 
 @stop
